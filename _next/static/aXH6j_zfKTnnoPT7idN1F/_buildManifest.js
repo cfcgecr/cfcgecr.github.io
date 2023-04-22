13099aa336d665c9c513253d7e078db8f8b01c82
niitish <71326553+niitish@users.noreply.github.com>
2023-04-22 12:53:19 +0530

    add previous repo build

---
+++ _next/static/aXH6j_zfKTnnoPT7idN1F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f894a573f264acc2.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],"/events":["static/chunks/pages/events-923c77485be92215.js"],sortedPages:["/","/_app","/_error","/events"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
